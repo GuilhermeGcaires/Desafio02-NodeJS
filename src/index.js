253189eb4f56de95f685a7fdb767e552d02fbe2e
GuilhermeGCaires <guilhermegcaires@gmail.com>
2022-03-15 01:25:19 -0300

    Desafio 2 Node concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({error: "User already exists"});
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(!user.pro && user.todos.length < 10){
+    return next();    
+  } else if (user.pro) {
+    return next();
+  } else {
+    return response.status(403).json({error: "Already at maximum todos capacity."});
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: "User doesn't exist."})
+  }
+
+  const uuidIsValid = validate(id);
+  if (!uuidIsValid){
+    return response.status(400).json({ error: "Id is not valid"});
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo){
+    return response.status(404).json({ error: "Todo does not exist."});
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  if(!id) return response.status(400).json({ error: "Id required"});
+
+  const user = users.find((user) => user.id === id);
+
+  if(!user) return response.status(404).json({error: "User doesn't exist"});
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
